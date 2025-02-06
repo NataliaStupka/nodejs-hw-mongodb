9b217c5734bb3e7afe835f257b207cca36f0cde9
NataliaStupka <84662491+NataliaStupka@users.noreply.github.com>
2025-02-06 22:56:42 +0200

    connection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js'; //база данних MongoDB
 
-//ініціалізація підключення до бази даних
-const bootstrap = async () => {
-  await initMongoConnection(); //підключення до бази даних в MONGODB
+// //ініціалізація підключення до бази даних
+// const bootstrap = async () => {
+//   await initMongoConnection(); //підключення до бази даних в MONGODB
 
-  setupServer(); //запуск сервера
-};
+//   setupServer(); //запуск сервера
+// };
 
-bootstrap();
+// bootstrap();
+
+await initMongoConnection();
+setupServer();
