f8e82322191cb9713bf77a529629c6281f9beadc
NataliaStupka <84662491+NataliaStupka@users.noreply.github.com>
2025-02-15 21:54:28 +0200

    const token

--- src/constants/env.js
+++ src/constants/env.js
@@ -1,3 +1,8 @@
 export const ENV_VARS = {
   PORT: 'PORT',
 };
+
+//sessions
+//export const ACCESS_TOKEN = 1000; //для тесту, при не робочому токену
+export const ACCESS_TOKEN = 15 * 60 * 1000; //15 minutes
+export const REFRESH_TOKEN = 24 * 60 * 60 * 30 * 1000; // 30 days
