2fc6a45ebe8f8d08901547f25a7fdde0f10a6f5e
NataliaStupka <84662491+NataliaStupka@users.noreply.github.com>
2025-02-15 21:45:59 +0200

    parseFilter default value

--- src/utils/parseFilterParams.js
+++ src/utils/parseFilterParams.js
@@ -7,7 +7,8 @@ const parseBoolean = (string) => {
   if (['true', 'false'].includes(string)) return JSON.parse(string); //буль
 };
 
-export const parseFilters = (filter) => {
+//по дефолту відсутність фільтрив (щоб не зламалось при запиті), хоча працювало і без дефолту
+export const parseFilters = (filter = {}) => {
   return {
     contactType: parseType(filter['filter(type)']), // Дістаємо значення type //значення по ключу "filter(type)"
     isFavourite: parseBoolean(filter['filter(isFavorite)']), // Дістаємо значення isFavorite
