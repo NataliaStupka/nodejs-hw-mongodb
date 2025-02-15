0d81d912fec16597fdcb02c7fcdf9176031e53a6
NataliaStupka <84662491+NataliaStupka@users.noreply.github.com>
2025-02-15 21:51:15 +0200

    authentificate

---
+++ src/middlewares/authenticate.js
@@ -0,0 +1,57 @@
+//аунтефікація (без авторизації)
+//перевіряє чи є такий користувач, якщо є то запускаємо його
+
+import createHttpError from 'http-errors';
+import { SessionsCollection } from '../db/models/session.js';
+import { UserCollection } from '../db/models/user.js';
+
+export const authentificate = async (req, res, next) => {
+  //кладемо відповідний хедер
+  const authHeader = req.get('Authorization');
+
+  try {
+    if (!authHeader) {
+      throw new createHttpError(401, 'No Authorization header provider');
+    }
+    //отримуємо токен
+    const [bearer, token] = authHeader.split(' ');
+
+    if (bearer !== 'Bearer') {
+      throw new createHttpError(
+        401,
+        'Authorization header should be of Bearer type!',
+      );
+    }
+    //якщо токен не прийшов
+    if (!token) {
+      throw new createHttpError(401, 'No Access token provided!');
+    }
+    //перевірка на наявність сесії
+    const session = await SessionsCollection.findOne({ accessToken: token }); //шукаємо по access token
+    if (!session) {
+      //якщо немає session
+      throw new createHttpError(401, 'No active session found!');
+    }
+
+    //чи валідний токен, чи вже 'протух'
+    if (session.accessTokenValidUntil < new Date()) {
+      throw new createHttpError(401, 'Access token expired!');
+    }
+
+    const user = await UserCollection.findById(session.userId);
+
+    if (!user) {
+      //якщо немає user - видаляємо сесію
+      await SessionsCollection.findByIdAndDelete(session._id);
+      throw new createHttpError(401, 'No user found for such session!');
+    }
+
+    //req.locals.user = user;
+    req.user = user;
+    console.log('midlewar-auth_user:', user);
+
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
