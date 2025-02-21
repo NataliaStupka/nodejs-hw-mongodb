a0b2505bec3951b9662fe0017d93604ef007e9f8
NataliaStupka <84662491+NataliaStupka@users.noreply.github.com>
2025-02-21 09:31:09 +0200

    sendEmail

---
+++ src/utils/sendEmail.js
@@ -0,0 +1,30 @@
+import nodemailer from 'nodemailer'; //надсилання листів.
+import createHttpError from 'http-errors';
+import { getEnvVar } from './getEnvVar'; //зчитування змінних оточення
+import { ENV_VARS } from '../constants/env';
+
+// createTransport - до якого сервісу посилаємо повідомлення
+const transporter = nodemailer.createTransporter({
+  host: getEnvVar(ENV_VARS.SMTP_HOST),
+  port: Number(getEnvVar(ENV_VARS.SMTP_PORT)),
+  auth: {
+    user: getEnvVar(ENV_VARS.SMTP_USER),
+    pass: getEnvVar(ENV_VARS.SMTP_PASSWORD),
+  },
+});
+
+// option - {to (отримувач), subject, html, from, ...}
+export const sendEmail = async (options) => {
+  //пробуємо надіслати email
+  try {
+    return await transporter.sendMail({
+      to: options.to,
+      subject: options.subject,
+      from: options.from,
+      html: options.html,
+    });
+  } catch (err) {
+    console.error(err);
+    return createHttpError(500, 'Failed to send an email');
+  }
+};
