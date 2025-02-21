ebe0c3950101956feb3572facf0818e0ba60a88c
NataliaStupka <84662491+NataliaStupka@users.noreply.github.com>
2025-02-21 09:13:47 +0200

    rout to src/templates

---
+++ src/constants/path.js
@@ -0,0 +1,6 @@
+//шляхи до різних файлів
+
+import path from 'node:path';
+
+// шлях до src/templates
+export const TEMPLATES_DIR_PATH = path.join(process.cwd(), 'src', 'templates'); //шаблон html лдя листа request password
