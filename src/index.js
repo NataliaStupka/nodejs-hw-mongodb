9283d818e80ba5ff0d3ca8d6f44a442a8c9b0a07
NataliaStupka <84662491+NataliaStupka@users.noreply.github.com>
2025-02-23 20:47:25 +0200

    change name create - make

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js'; //база данних MongoDB
 import { TEMP_DIR_PATH, UPLOADS_DIR_PATH } from './constants/path.js'; //шлях до src/temp, src/uploads
-import { createDirIfNotExist } from './utils/CreateDirIfNotExist.js';
+import { createDirIfNotExist } from './utils/makeDirIfNotExist.js';
 
 //при запуску додатку створює папку якщо її ще не існує
 await createDirIfNotExist(TEMP_DIR_PATH);
